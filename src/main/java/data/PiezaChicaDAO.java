1f5aee93ce426299654838803f645b80fc9eff64
Rakso <oskarr.magana@gmail.com>
2021-06-10 02:02:23 -0500

    Updated DAOs

--- src/main/java/data/PiezaChicaDAO.java
+++ src/main/java/data/PiezaChicaDAO.java
@@ -1,6 +1,5 @@
 package data;
-
-import entity.LowerBody;
+import entity.PiezaChica;
 import java.util.ArrayList;
 import java.sql.Connection;
 import java.sql.Statement;
@@ -8,29 +7,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class LowerBodyDAO implements DAO<LowerBody> {
+public class PiezaChicaDAO implements DAO<PiezaChica> {
     @Override
-    public ArrayList<LowerBody> selectQuery() throws SQLException {
+    public ArrayList<PiezaChica> selectQuery() throws SQLException {
         Connection conn = Conexion.connect();
         Statement st = conn.createStatement();
-        ResultSet rs = st.executeQuery("SELECT id, linea, color, (medidas).largo, (medidas).ancho, (medidas).alto, r_fabricante, r_armadura, altura_suelo FROM lower_body");
-        ArrayList<LowerBody> piezas = new ArrayList<>();
-        while (rs.next())
-            piezas.add(new LowerBody(rs.getInt("id"), rs.getString("linea"), rs.getString("color"), rs.getDouble("largo"), rs.getDouble("ancho"), rs.getDouble("alto"), rs.getString("r_armadura"), rs.getInt("r_armadura"), rs.getDouble("altura_suelo")));
+        ResultSet rs = st.executeQuery("SELECT id, linea, color, (medidas).largo, (medidas).ancho, (medidas).alto, r_fabricante, r_armadura FROM pieza_chica");
+        ArrayList<PiezaChica> piezas_chicas = new ArrayList<>();
+        while(rs.next())
+            piezas_chicas.add(new PiezaChica(rs.getInt("id"), rs.getString("linea"), rs.getString("color"), rs.getDouble("largo"), rs.getDouble("ancho"), rs.getDouble("alto"), rs.getString("r_fabricante"), rs.getInt("r_armadura")));
 
         Conexion.close(rs);
         Conexion.close(st);
         Conexion.close(conn);
-
-        return piezas;
+        return piezas_chicas;
     }
 
     @Override
-    public void insertUpdate(LowerBody element) throws SQLException {
+    public void insertUpdate(PiezaChica element) throws SQLException {
         if(element == null)
             return;
         Connection conn = Conexion.connect();
-        PreparedStatement st = conn.prepareStatement("INSERT INTO lower_body(linea, color, medidas, r_fabricante, r_armadura, altura_suelo) VALUES (?, ?, row(?,?,?), ?, ?, ?)");
+        PreparedStatement st = conn.prepareStatement("INSERT INTO pieza_chica(linea, color, medidas, r_fabricante, r_armadura) VALUES (?, ?, row ( ?,?,? ), ?, ?)");
         st.setString(1, element.getLinea());
         st.setString(2, element.getColor());
         st.setDouble(3, element.getLargo());
@@ -38,7 +36,6 @@ public class LowerBodyDAO implements DAO<LowerBody> {
         st.setDouble(5, element.getAlto());
         st.setString(6, element.getR_fabricante());
         st.setInt(7, element.getR_armadura());
-        st.setDouble(8, element.getAltura_suelo());
         st.executeUpdate();
 
         Conexion.close(st);
@@ -46,11 +43,11 @@ public class LowerBodyDAO implements DAO<LowerBody> {
     }
 
     @Override
-    public void update(LowerBody element) throws SQLException {
+    public void update(PiezaChica element) throws SQLException {
         if(element == null)
             return;
         Connection conn = Conexion.connect();
-        PreparedStatement st = conn.prepareStatement("UPDATE lower_body SET linea = ?, color = ?, medidas = row(?,?,?), r_fabricante = ?, r_armadura = ?, altura_suelo = ? WHERE id = ?");
+        PreparedStatement st = conn.prepareStatement("UPDATE pieza_chica SET linea = ?, color = ?, medidas = row(?, ? ,?), r_armadura = ?, r_fabricante = ? WHERE id = ?");
         st.setString(1, element.getLinea());
         st.setString(2, element.getColor());
         st.setDouble(3, element.getLargo());
@@ -58,8 +55,7 @@ public class LowerBodyDAO implements DAO<LowerBody> {
         st.setDouble(5, element.getAlto());
         st.setString(6, element.getR_fabricante());
         st.setInt(7, element.getR_armadura());
-        st.setDouble(8, element.getAltura_suelo());
-        st.setInt(9, element.getId());
+        st.setInt(8, element.getId());
         st.executeUpdate();
 
         Conexion.close(st);
@@ -67,12 +63,12 @@ public class LowerBodyDAO implements DAO<LowerBody> {
     }
 
     @Override
-    public void deleteUpdate(LowerBody element) throws SQLException {
+    public void deleteUpdate(PiezaChica element) throws SQLException {
         if(element == null)
             return;
         Connection conn = Conexion.connect();
         Statement st = conn.createStatement();
-        st.executeUpdate("DELETE FROM lower_body WHERE id = " + element.getId());
+        st.executeUpdate("DELETE FROM pieza_chica WHERE id = " + element.getId());
 
         Conexion.close(st);
         Conexion.close(conn);
